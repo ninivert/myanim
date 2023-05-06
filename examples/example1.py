c647b2899d54194a011b645052499a8858cc1d86
Nicole Vadot <ninivert1@gmail.com>
2023-05-06 22:16:38 +0200

    hehe i cant spell

--- examples/example1.py
+++ examples/example1.py
@@ -7,7 +7,7 @@ import sys
 sys.path.append('..')
 from anim import *
 
-disk = Disc(
+disc = Disc(
 	xy=Var(Vec([300, 200])), r=Var(50),
 	rgba=Var(Vec((0/255, 181/255, 204/255, 1)))
 )
@@ -15,7 +15,7 @@ rect = Rect(
 	xy=Var(Vec([100, 380])), w=Var(40), h=Var(60),
 	rgba=Var(Vec((249/255, 180/255, 45/255, 1)))
 )
-disk2 = Disc(
+disc2 = Disc(
 	xy=Var(Vec([30, 250])), r=Var(10),
 	rgba=Var(Vec((50/255, 205/255, 50/255, 1)))
 )
@@ -31,8 +31,8 @@ rect3 = Rect(
 w, h = 500, 500
 background = Rect(Var(Vec([0.0, 0.0])), Var(w), Var(h), rgba=Var((0.05, 0.05, 0.05, 1)))
 
-grow_radius = Transition(var=disk.r,
-	initial=disk.r.value, final=disk.r.value+100,
+grow_radius = Transition(var=disc.r,
+	initial=disc.r.value, final=disc.r.value+100,
 	start=1.05, length=1,
 	tween=ease_in_out_quad
 )
@@ -41,8 +41,8 @@ translate_x = Transition(var=rect.xy,
 	start=0.5, length=2,
 	tween=ease_out_back
 )
-oscillate_y = Transition(var=disk2.xy,
-	initial=Vec([disk2.xy.value[0], disk2.xy.value[1]-100]), final=Vec([disk2.xy.value[0], disk2.xy.value[1]+100]),
+oscillate_y = Transition(var=disc2.xy,
+	initial=Vec([disc2.xy.value[0], disc2.xy.value[1]-100]), final=Vec([disc2.xy.value[0], disc2.xy.value[1]+100]),
 	start=1, length=4,
 	tween=lambda s: (math.sin(s * 3 *2*math.pi)+1)/2
 )
@@ -51,27 +51,24 @@ translate_line = Transition(var=rect2.xy,
 	start=1.4, length=2,
 	tween=ease_in_out_trig,
 )
-green_to_pink = Transition(var=disk2.rgba,
-	initial=disk2.rgba.value, final=Vec((255/255, 20/255, 147/255, 1)),
+green_to_pink = Transition(var=disc2.rgba,
+	initial=disc2.rgba.value, final=Vec((255/255, 20/255, 147/255, 1)),
 	start=1, length=4
 )
-translate_bezier = Transition(var=rect3.xy,
+translate_bquad = Transition(var=rect3.xy,
 	initial=rect3.xy.value, final=Vec((100, 200)),
 	start=1.8, length=1,
 	interp=QuadraticBezier(Vec([230, -50]))
 )
-translate_bezier3 = Transition(var=rect3.xy,
-	initial=translate_bezier.final, final=Vec((470, 430)),
-	start=translate_bezier.end, length=1.4,
+translate_bcub = Transition(var=rect3.xy,
+	initial=translate_bquad.final, final=Vec((470, 430)),
+	start=translate_bquad.end, length=1.4,
 	interp=CubicBezier(Vec((20, 600)), Vec((570, 25)))
 )
 
-# TODO : chain transitions
-
-
 anim = Animation(
-	actors=[background, disk, rect, disk2, rect2, rect3],
-	transitions=[grow_radius, translate_x, oscillate_y, translate_line, green_to_pink, translate_bezier, translate_bezier3],
+	actors=[background, disc, rect, disc2, rect2, rect3],
+	transitions=[grow_radius, translate_x, oscillate_y, translate_line, green_to_pink, translate_bquad, translate_bcub],
 	framerate=60,
 	width=w, height=h
 )
