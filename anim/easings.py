7a2a1a9119e7a7baf1d219fc0d4ede5357e495c4
Nicole Vadot <nicole@nvadot.dev>
2023-09-03 18:55:39 +0200

    fixed bug with ease_out_bounce

--- anim/easings.py
+++ anim/easings.py
@@ -34,7 +34,7 @@ def ease_in_cubic(t: float) -> float:
 	return t**3
 
 def ease_out_cubic(t: float) -> float:
-	return (t-1)**3 + 1  # TODO : fixme
+	return (t-1)**3 + 1
 
 def ease_in_out_cubic(t: float) -> float:
 	return 4*t**3 if t < 0.5 else 1 - (-2*t+2)**3 / 2
@@ -82,17 +82,16 @@ def ease_in_out_back(t: float) -> float:
 	return 4*t**2 * ((b+1)*t - 0.5*b) if t < 0.5 else (2*t-2)**2 * ((b+1)*(t-1)+0.5*b) + 1
 
 def ease_out_bounce(t: float) -> float:
-	# FIXME : this doesn't work
 	a = 7.5625
 	b = 2.75
 	if t < 1/b:
 		return a*t**2
 	elif t < 2/b:
-		return a*((t-1.5)/b)*t + 0.75
+		return a*(t-1.5/b)*(t-1.5/b) + 0.75
 	elif t < 2.5/b:
-		return a*((t-2.25)/b)*t + 0.9375
+		return a*(t-2.25/b)*(t-2.25/b) + 0.9375
 	else:
-		return a*((t-2.625)/b)*t + 0.984375
+		return a*(t-2.625/b)*(t-2.625/b) + 0.984375
 
 def ease_in_bounce(t: float) -> float:
 	return 1 - ease_out_bounce(t)
